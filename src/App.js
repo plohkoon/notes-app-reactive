df747642a38a0346f6cf42e7fe3c9f13f3d053f4
plohkoon <greg.huber@outlook.com>
2019-04-10 16:27:37 -0700

    Added basic date control and rendering just to start testing functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
+//various react and material UI imports
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withStyles } from '@material-ui/core/styles';
+//my component imports
+import TopBar from './Components/TopBar.js';
+import Content from './Components/Content.js';
+//gets the ipcRenderer for requests the electron main process
+const { ipcRenderer } = window.require('electron');
 
 class App extends Component {
+  //constructor to set initial
+  constructor(props) {
+    super(props);
+    //sets initial date and notes
+    this.state = {
+      currentDate: this.getDate(),
+      currentNotes: [],
+    }
+    //gets rows
+    this.getRows()
+  }
+
+  //A simple function to get todays date in the proper format
+  //pulled from initial notes app
+  getDate = (date) => {
+
+      let day, month, year;
+
+      date = date ? date : new Date();
+      day = ("0" + (date.getDate())).slice(-2);
+      month = ("0" + (date.getMonth() + 1)).slice(-2);
+      year = date.getFullYear();
+
+      return year + "-" + month + "-" + day;
+
+  }
+  //changes the date state and than regets rows
+  onDateChange = (date) => {
+    this.setState({currentDate: this.getDate(date)});
+    this.getRows();
+  }
+
+  getRows = () => {
+    //sends a getRows query
+    ipcRenderer.send('getRows', this.state.currentDate);
+    //when sendRows gets recieved sets the current state
+    ipcRenderer.on('sendRows', (event, arg) => {
+      console.log(arg);
+      this.setState({currentNotes: arg});
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <TopBar
+          date={this.state.currentDate}
+          dateChange={this.onDateChange}
+          getRows={this.getRows}
+        />
+        <Content
+          date={this.state.currentDate}
+          notes={this.state.currentNotes}
+          getRows={this.getRows}
+        />
       </div>
     );
   }
