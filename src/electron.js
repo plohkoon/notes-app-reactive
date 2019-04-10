350496e65ec13e0797c01aa9e440b68267075ce0
plohkoon <greg.huber@outlook.com>
2019-04-10 16:26:42 -0700

    Added DB open and basis for all queries

--- src/electron.js
+++ src/electron.js
@@ -5,10 +5,102 @@ const BrowserWindow = electron.BrowserWindow;
 const path = require('path');
 const url = require('url');
 const isDev = require('electron-is-dev');
+const { ipcMain } = require('electron');
 
-let mainWindow;
+const fs = require('fs'),
+      sqlite = require('sqlite3'),
+      util = require('util');
 
-function createWindow() {
+
+//gets the number of day differences between 2 dates
+const dateDiff = (origin, current) => {
+    //converts string to date objects
+    const   originDate = new Date(origin),
+            currentDate = new Date(current),
+
+            originTime = originDate.getTime(),
+            currentTime = currentDate.getTime(),
+            //gets the difference between times and then converts it to days
+            diffTime = currentTime - originTime,
+            diffDay = Math.round(diffTime / (1000 * 60 * 60 * 24));
+
+    console.log(originDate);
+    console.log(currentDate);
+    console.log(diffDay);
+    //if the day is after last track returns it
+    if(diffDay >= 0) {
+        return diffDay.toString();
+    }
+    else {
+        return "n/a";
+    }
+
+}
+//simple function to clean sql query
+const sqlEscape = (query) => {
+    //changes how apostrphe escaping is handled as SQL does differently
+    let     escapedQuery = query.replace("\'", "''"),
+            cleansedQuery = sqlstring.escape(escapedQuery);
+    //cleans up after the sql escape gets a bit to OCD in its cleanse
+    cleansedQuery = cleansedQuery.replace(/\\/g, "");
+    console.log(cleansedQuery);
+
+    return cleansedQuery;
+
+}
+
+let mainWindow,
+    db;
+
+async function prepareDB() {
+  return new Promise((resolve, reject) => {
+    //pomisifies the database creation and writeFile function
+    const writeFile = util.promisify(fs.writeFile),
+          Database = (dbPath, sqlType) => {
+            return new Promise((res, rej) => {
+              //creates the database
+              let initializedDB = new sqlite.Database(dbPath, sqlType, (err) => {
+                //if errored then rejects promise
+                if(err) {
+                  console.log("failed to intialize databse connection");
+                  rej(err);
+                }
+                //if succeeds returns database
+                res(initializedDB);
+              });
+            });
+          };
+
+      let dbPath = electron.app.getPath('userData') + "/NOTES.db";
+
+      writeFile(dbPath,  "", { flag : 'wx' })
+        .catch(err => {
+          console.log("file already there");
+        })
+        .then(res => {
+          console.log("file exists");
+          Database(dbPath, sqlite.OPEN_READWRITE)
+            .then(db => {
+              console.log("database exists");
+              db.all("create table  if not exists notes(id INTEGER PRIMARY KEY AUTOINCREMENT, note_id TEXT, note TEXT, date TEXT);", [], (err, res) => {
+                if(err) {
+                  console.log(err);
+                  throw err;
+                }
+                resolve(db);
+              })
+            })
+            .catch(err => {
+              console.log("there was an error with the database")
+            })
+        })
+
+  });
+
+}
+
+async function createWindow() {
+  db = await prepareDB();
   mainWindow = new BrowserWindow({width: 900, height: 680});
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
@@ -26,4 +118,46 @@ app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
   }
+});
+
+ipcMain.on('getRows', (event, arg) => {
+  let query="select * from notes where date=? order by note_id";
+  console.log(arg);
+  db.all(query, arg, (err, rows) => {
+    if(err) {
+      console.log(err);
+    }
+    event.sender.send('sendRows', rows);
+  });
+});
+
+ipcMain.on('addRow', (event, arg) => {
+  let query="insert into notes (date, note_id, note) values (?,?,?);"
+  let values = JSON.parse(arg);
+  db.all(query, values, (err, res) => {
+    if(err) {
+      console.log(err);
+    }
+    event.sender.send('rowAdded', true);
+  });
+});
+
+ipcMain.on('deleteRow', (event, args) => {
+  let query="delete from notes where date=? and id=?;";
+  db.all(query, [], (err, res) => {
+    if(err) {
+      console.log(err);
+    }
+
+  });
+});
+
+ipcMain.on('changeRow', (event, args) => {
+  let query="update notes set note=? where date=? and id=?";
+  db.all(query, [], (err, res) => {
+    if(err) {
+      console.log(err);
+    }
+
+  });
 });
