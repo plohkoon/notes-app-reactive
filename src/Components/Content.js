c65679b40d59feb3f0af65eecb492ca52515297c
plohkoon <greg.huber@outlook.com>
2019-04-11 15:43:37 -0700

    turned content into giant flexbox

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 //personal imports
 import NoteCard from './NoteCard.js';
+//importing styles
+import '../styles/contentStyles.css'
 
 export default class Content extends Component {
   //maps all the NoteCards with their respective note
   render() {
     return (
-      <div>
+      <div className='contentContainer'>
         {this.props.notes.map(row => {
           return (
             <NoteCard
