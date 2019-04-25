e7f350815674e7f6e366ae3d88689212e0043dc6
plohkoon <greg.huber@outlook.com>
2019-04-24 18:47:17 -0700

    Revert "added basic stylings, logic, and layout for the ScopeApp portion of the page"
    This reverts commit fc42719f3ad4d2a5cb0bb5c31d849194f40ae859.

--- src/Components/ScopeApp/ScopeApp.js
+++ src/Components/ScopeApp/ScopeApp.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
-import Fab from '@material-ui/core/Fab';
-
-import ArrowBackIos from '@material-ui/icons/ArrowBackIos';
-import ArrowForwardIos from '@material-ui/icons/ArrowForwardIos';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import {Link} from 'wouter';
 
 import TopBar from './TopBar/TopBar';
-import Content from './Content/Content';
-
-import {scopeData} from './scopeData';
-
-import './styles.css';
 
 export default class ScopeApp extends Component {
 
@@ -17,23 +11,6 @@ export default class ScopeApp extends Component {
     return(
       <div>
         <TopBar />
-        <Content
-          data={scopeData[1]}
-        />
-        <div className='controlButtons'>
-          <Fab
-            className='directBut'
-            color='primary'
-          >
-            <ArrowBackIos />
-          </Fab>
-          <Fab
-            className='directBut'
-            color='primary'
-          >
-            <ArrowForwardIos />
-          </Fab>
-        </div>
       </div>
     )
   }
